656f7f27bb9d21c32606329bfd0abb0c3d5876bb
Walter Zimmerman <zimmermanw84@gmail.com>
2015-04-24 12:02:07 -0700

    parse config JSON production test

--- models/index.js
+++ models/index.js
@@ -6,7 +6,7 @@ var Sequelize = require('sequelize');
 var basename  = path.basename(module.filename);
 var env       = process.env.NODE_ENV || 'development';
 var config    = require(__dirname + '/../config/config.json')[env];
-var sequelize = new Sequelize(config.database, config.username, config.password, config);
+var sequelize = new Sequelize(JSON.parse(config.database), JSON.parse(config.username), JSON.parse(config.password), config);
 var db        = {};
 
 fs
